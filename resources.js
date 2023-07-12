ff0255dc3a875e6b1f7e27cf02776bbcd32b2b88
kittykitcat <80047018+kittykitcat@users.noreply.github.com>
2023-07-12 14:04:21 +0200

    Add files via upload

---
+++ resources.js
@@ -0,0 +1,28 @@
+define([
+    'js/system/globals'
+], function (
+    globals
+) {
+    return {
+        sprites: {},
+
+        init: async function () {
+            const { sprites } = this;
+            const { clientConfig: { resourceList, textureList } } = globals;
+
+            const fullList = [].concat(resourceList, textureList);
+
+            return Promise.all(fullList.map(s => {
+                return new Promise(res => {
+                    const spriteSource = s.includes('.png') ? s : `images/${s}.png`;
+
+                    const sprite = new Image();
+                    sprites[s] = sprite;
+                    sprite.onload = res;
+                    sprite.src = spriteSource;
+                    sprite.crossOrigin = 'anonymous';
+                });
+            }));
+        }
+    };
+});
